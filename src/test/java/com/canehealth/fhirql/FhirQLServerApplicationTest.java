29b9326621c9664399deeace0b67ba3eef1655e1
beapen <github@gulfdoctor.net>
2021-04-13 11:33:07 -0400

    start

---
+++ src/test/java/com/canehealth/fhirql/FhirQLServerApplicationTest.java
@@ -0,0 +1,35 @@
+package com.canehealth.fhirql;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class FhirQLServerApplicationTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void metadata() {
+        ResponseEntity<String> entity = this.restTemplate.getForEntity("/fhir/metadata", String.class);
+        assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(entity.getBody()).contains("\"status\": \"active\"");
+    }
+
+    @Test
+    public void patientResource() {
+        ResponseEntity<String> entity = this.restTemplate.getForEntity("/fhir/Patient/1", String.class);
+        assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(entity.getBody()).contains("\"family\": \"Van Houte\"");
+    }
+}
