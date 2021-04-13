03b15d09216302bfd4b5a4bd82eab4da92ba7950
beapen <github@gulfdoctor.net>
2021-04-13 13:02:16 -0400

    debug DefaultController

--- src/main/java/com/canehealth/fhirql/controller/DefaultController.java
+++ src/main/java/com/canehealth/fhirql/controller/DefaultController.java
@@ -5,10 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Controller
-public class DefaultController extends WebMvcConfigurerAdapter {
+public class DefaultController {
 
 
     @RequestMapping(value = {"/editor"}, method = RequestMethod.GET)
