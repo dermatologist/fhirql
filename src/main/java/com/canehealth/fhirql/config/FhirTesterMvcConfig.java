516e003922ea5e8db46afb1d59ff6985537f602b
beapen <github@gulfdoctor.net>
2021-04-13 12:25:48 -0400

    Add MVCconfig

---
+++ src/main/java/com/canehealth/fhirql/config/FhirTesterMvcConfig.java
@@ -0,0 +1,30 @@
+package com.canehealth.fhirql.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "ca.uhn.fhir.to" }, excludeFilters = {
+@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = ca.uhn.fhir.to.FhirTesterMvcConfig.class) })
+public class FhirTesterMvcConfig extends WebMvcConfigurerAdapter {
+    public FhirTesterMvcConfig() {
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/css/");
+        registry.addResourceHandler("/fa/**").addResourceLocations("classpath:/fa/");
+        registry.addResourceHandler("/fonts/**").addResourceLocations("classpath:/fonts/");
+        registry.addResourceHandler("/img/**").addResourceLocations("classpath:/img/");
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/js/");
+        registry.addResourceHandler("/profiles/**").addResourceLocations("classpath:/profiles/");
+
+    }
+
+
+}
